2c1b1690040e8d02c654cd157bed6395345cdbcd
Tim Sarkar <timsarkar@gmail.com>
2024-10-26 23:33:35 -0400

    Add files via upload
    Main code file(Had issues getting test file to work as pytest and venv folder wouldnt work)

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,20 @@
 """
 Validates strings as palindromes.
 """
+from collections import deque
+
+def is_palindrome(x):
+   d= deque(x)
+   if type(x) is not str:
+      raise ValueError
+   while len(d) >1:
+           if d.popleft() != d.pop():
+             return False
+
+   return True
+
+
+
+
+
+print(is_palindrome("radar"))
