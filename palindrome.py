353d7afc31c86c851b08bd6b86a3b7dac7097d88
Tim Sarkar <timsarkar@gmail.com>
2024-10-26 23:41:14 -0400

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -1,20 +1,19 @@
 """
 Validates strings as palindromes.
 """
+
 from collections import deque
-
 def is_palindrome(x):
-   d= deque(x)
-   if type(x) is not str:
-      raise ValueError
-   while len(d) >1:
-           if d.popleft() != d.pop():
-             return False
-
-   return True
-
-
+    if type(x) is not str:
+        raise ValueError
+    if x == "":
+        return False
+    d= deque(x)
+    while len(d) > 1:
+        if d.popleft() != d.pop():
+            return False
 
 
+    return True
 
 print(is_palindrome("radar"))
